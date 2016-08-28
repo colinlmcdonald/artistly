47c77a68b6bb25eb34d83a4fc963e7e504d44ebe
Colin McDonald <mickydwow@gmail.com>
2016-08-27 17:38:48 -0700

    Changed picture viewing

--- public/app.js
+++ public/app.js
@@ -1,4 +1,5 @@
 $( document ).ready(function() {
+  var __errors__ = {};
   $('#submit-artist').submit(function(event) {
     event.preventDefault();
     var artist = $('#input-artist').val();
@@ -19,45 +20,62 @@ $( document ).ready(function() {
       requestSpotifyArtist(artist);
     }
   });
-});
-
-function errorMessage(msg, picture) {
-  var $error = document.createElement('div');
-  $error.innerText = msg;
-  $error.className = 'error-message';
-  if (picture) {
-    $('#artist-picture').html($error);
-  } else {
-    $("#artist-info").html($error);
-    $('#artist-picture').html('');
-  }
-};
-
-function clearError() {
-  $('#artist-info').html('');
-  $('#artist-picture').html('');
-};
-
-function requestSpotifyArtist(artist) {
-  artist = encodeURI(artist);
-  $.get('https://api.spotify.com/v1/search?q=' + artist + '&type=artist', function(data) {
-    if (data.artists.items.length) {
-      processData(data.artists.items[0]);
+  function errorMessage(msg, picture) {
+    var $error = document.createElement('div');
+    $error.innerText = msg;
+    $error.className = 'error-message';
+    if (picture) {
+      $('#artist-picture').html($error);
     } else {
-      __errors__['no-artist'] = true;
-      errorMessage('Sorry, no artists matched that search.')
+      $("#artist-info").html($error);
+      $('#artist-picture').html('');
     }
-  });
-};
+  };
 
-function processData(data) {
-  var name = _.template("<h3><%=name%></h3>");
-  var picture = _.template("<img src='<%=image%>' class='img-fluid'>")
-  $("#artist-info").html(name({name: data.name}));
-  if (!data.images.length) {
-    __errors__['no-image'] = false;
-    errorMessage('Sorry, there is no image for this search.', true)
-  } else {
-    $("#artist-picture").html(picture({image: data.images[0].url}))
-  }
-};
+  function clearError() {
+    $('#artist-info').html('');
+    $('#artist-picture').html('');
+  };
+
+  function requestSpotifyArtist(artist) {
+    artist = encodeURI(artist);
+    $.get('https://api.spotify.com/v1/search?q=' + artist + '&type=artist', function(data) {
+      if (data.artists.items.length) {
+        processData(data.artists.items);
+      } else {
+        __errors__['no-artist'] = true;
+        errorMessage('Sorry, no artists matched this search.')
+      }
+    });
+  };
+
+  function processData(data) {
+      console.log(data);
+    var search = _.template([
+      "<div class='container'>",
+      "<% _.each(artists, function(artist, i) { %>" +
+        "<% if (i === 0 || i % 4 === 0) { %>" +
+          "<div class='row'></div>" +
+        "<% } %>" +
+          "<div class='col-xs-3' id='artists'>" +
+            "<p class='artist-name'><%= artist.name %></p>" + 
+            "<% if(artist.images.length) { %>" +
+              "<img src=<%= artist.images[0].url %> class='img-fluid' id='artist-image' + <% i %> alt='No Image Available'>" +  
+            "<% } else { %>" +
+              "<img src='no_image_available.jpeg' class='img-fluid'>" +
+            "<% } %>" +
+          "</div>" +
+      "<% }) %>",
+      "</div>",
+      "</div>"
+    ].join('\n'))
+    $("#artist-info").html(search({artists: data}));
+    // var picture = _.template("<img src='<%=image%>' class='img-fluid'>")
+    // if (!data.images.length) {
+    //   __errors__['no-image'] = false;
+    //   errorMessage('Sorry, there is no image for this search.', true)
+    // } else {
+    //   $("#artist-picture").html(picture({image: data.images[0].url}))
+    // }
+  };
+});
